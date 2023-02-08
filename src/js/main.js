d36ac8cc1a6511cb4241f44a0eaf1302cc9924d6
Vladislav Tokarev <vladtokarev127@gmail.com>
2023-02-08 12:51:06 +0500

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+$(function() {
+
+	$('[data-show-map]').click(function(e) {
+		e.preventDefault();
+		$('[data-map]').slideToggle(300);
+		$(this).find('span').toggle();
+	});
+
+	$('[data-copy]').click(function(e) {
+		e.preventDefault();
+		$(this).addClass('is-active');
+		$(this).find('span:last-child').show();
+		$(this).find('span:first-child').hide();
+		setTimeout(() => {
+			$(this).find('span:last-child').hide();
+			$(this).find('span:first-child').show();
+			$(this).removeClass('is-active');
+		}, 2500);
+		let copyText = $(this).data('copy');
+		let $temp = $('<input>');
+		$('body').append($temp);
+		$temp.val(copyText).select();
+		document.execCommand('copy');
+		$temp.remove();
+	});
+
+});
