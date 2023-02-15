e07aa2c1312bb6fa4dfa8901538d20bf36fd7bae
Vladislav Tokarev <vladtokarev127@gmail.com>
2023-02-15 16:42:25 +0500

    done header

--- src/js/main.js
+++ src/js/main.js
@@ -24,100 +24,58 @@ $(function() {
 		$temp.remove();
 	});
 
+	$('.header__search-link').click(function(e) {
+		e.preventDefault();
+		let parent = $(this).parents('.header__search');
+		parent.find('.header__search-form').slideToggle(300);
+	});
+
+	$('.js-search').on('keyup', function(e) {
+		let target = $(this);
+		let val = target.val();
+		let $delay = 500;
+		let parent = $(this).parents('.header__search-form');
+		clearTimeout(target.data('timer'));
+		target.data('timer', setTimeout(function() {
+			target.removeData('timer');
+			let items = parent.find('.header__search-item');
+			items.each(function() {
+				$(this).show();
+				let text = $(this).text();
+				if (text.indexOf(val) != -1) {
+					$(this).show();
+				} else {
+					$(this).hide();
+				}
+			})
+		}, $delay))
+	})
+
 	$('.station__schedule-day span').each(function() {
 		if($(this).text() === 'сб' || $(this).text() === 'вс') {
 			$(this).addClass('is-holiday');
 		}
 	});
 
-	Vue.createApp({
-		data: () => ({
-			search: '',
-			loading: true,
-			url: 'https://bus.tutu.ru/bus/v1/schedule/bus_terminal/?bus_stop_id=1177120&offset=0&limit=100',
-			departure: 1444796,
-			stations: [],
-			stationsNames: {},
-			data: [],
-			dataStations: []
-		}),
-		mounted () {
-			fetch(this.url)
-				.then(res => res.text())
-				.then((res) => {
-					this.data = JSON.parse(res).data;
-					let obj;
-					this.data.forEach(item => {
-						this.dataStations.push(item.geoPointId);
-						obj = {
-							arrival: ''+item.geoPointId,
-							schedules: []
-						};
-						item.schedules.forEach(schedule => {
-							let someObj = {
-								time: schedule.time,
-								days: schedule.activeDaysData.mode,
-								transit: schedule.isTransit,
-								activeDays: schedule.activeDaysData.params.activeDays ? schedule.activeDaysData.params.activeDays : schedule.activeDays
-							};
-							obj.schedules.push(someObj)
-						})
-						this.stations.push(obj)
-					});
+	if($(window).width() < 768) {
+		$('.menu-item-has-children a').click(function(e) {
+			e.preventDefault();
+			$(this).parent().toggleClass('is-active').find('ul').slideToggle(300);
+		})
+	} else {
+		$('.menu-item-has-children').hover(function() {
+			$(this).addClass('is-active').find('ul').slideDown(300);
+		}, function() {
+			$(this).removeClass('is-active').find('ul').slideUp(300);
+		});
+	}
+	
 
-					this.dataStationUnic.forEach(item => {
-						let geoUrl = `https://api-bus.tutu.ru/v1/search/?departureId=${this.departure}&arrivalId=${item}&departureDate=09.02.2023&seatCount=1`
-						fetch(geoUrl)
-							.then(res => res.text())
-							.then((res) => {
-								this.stationsNames[item] = JSON.parse(res).data.references.geoPoints[1].name;
-							})
-					});
-
-					this.init();
-				})
-		},
-		computed: {
-			dataStationUnic() {
-				let result = [];
-
-				result = this.dataStations.reduce((acc, item) => {
-					if (acc.includes(item)) {
-						return acc;
-					}
-					return [...acc, item];
-				}, [])
-
-				return result;
-			}
-		},
-		methods: {
-			searchFun: (e) => {
-				let target = e.target;
-				let val = target.value;
-				let $delay = 500;
-				clearTimeout(target.dataset.timer);
-				target.dataset.timer = setTimeout(() => {
-					target.dataset.timer = '';
-					let items = document.querySelectorAll('.station__item');
-					items.forEach((item) => {
-						item.style.display = 'flex';
-						let text = item.querySelector('.station__item-title span').textContent
-						console.log(val);
-						console.log(text);
-						console.log(text.indexOf(val));
-						if (text.indexOf(val) != -1) {
-							item.style.display = 'flex';
-						} else {
-							item.style.display = 'none';
-						}
-					})
-				}, $delay)
-			},
-			init: function() {
-
-			}
-		}
-	}).mount('#app');
+	$('.header__menu').click(function(e) {
+		e.preventDefault();
+		$(this).toggleClass('is-active');
+		$('.header__nav').toggleClass('is-active');
+		$('body').toggleClass('is-fixed');
+	})
 
 });
